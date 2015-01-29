6f91eea4410240b2ff6cdce0aafca3ab56411dda
Luis Mayta <slovacus@gmail.com>
2015-01-29 15:20:57 -0500

    added dependences install git

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,18 @@
-class git {
-    package { 'git':
-        ensure => installed,
-        require => Exec["manager update"],
-    }
+# == Class: git
+#
+# Full description of class module here.
+#
+# === Parameters
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#
+class git (
+  $package_name = $::git::params::package_name
+) inherits ::git::params {
+
+  # validate parameters here
+
+  class { '::git::install': } ->
+    Class['::git']
 }
